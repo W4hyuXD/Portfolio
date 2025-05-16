40dad84961b9e2fb3fc357e4366ff68e595b4f6d
WahyuDin Ambia XD <131398263+W4hyuXD@users.noreply.github.com>
2025-05-16 22:53:44 +0700

    Add files via upload

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,19 @@
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Projects from '../components/Projects';
+import Skills from '../components/Skills';
+import About from '../components/About';
+import Contact from '../components/Contact';
 
+export default function Home() {
+  return (
+    <main className="min-h-screen w-full bg-white text-black">
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Skills />
+      <About />
+      <Contact />
+    </main>
+  );
+}
