5c6b9849b6853dd32ac7836df7ffa512a3b6c5b5
WahyuDin Ambia XD <131398263+W4hyuXD@users.noreply.github.com>
2025-05-17 18:39:11 +0700

    Delete src/components/Footer.jsx

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-6 text-center bg-neutral-900 text-gray-500">
-      <p>&copy; {new Date().getFullYear()} Wahyu. All rights reserved.</p>
-    </footer>
-  );
-}
